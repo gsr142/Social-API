72dbd9d0ed267875672d71ec71b7d151cf152245
gsr142 <gsr142@gmail.com>
2023-08-24 15:55:06 -0700

    create user Model

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,7 @@
 const { connect, connection } = require('mongoose');
 
 
-const connectionString =
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/studentsDB';
+const connectionString = 'mongodb://127.0.0.1:27017/studentsDB';//make sure to change db name
 
 connect(connectionString);
 
---
+++ models/User.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require('mongoose');
+const thoughtSchema = require('./Thought');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/.+@.+\..+/, 'Must match an email address!']
+        },
+        thoughts: [thoughtSchema],
+        friends: [{ type: Schema.Types.ObjectId, ref: 'User' }]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    }
+)
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+const User = model('user', userSchema);
+
+module.exports = User;
