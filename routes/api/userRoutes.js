563eec4f5584011b48a2a5f5f063d3828d510d14
gsr142 <gsr142@gmail.com>
2023-08-25 16:39:26 -0700

    add route for get userbyID

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 
 const {
     getAllUsers,
+    getSingleUser,
     createUser
 } = require('../../controllers/userController');
 
 router.route('/').get(getAllUsers).post(createUser);
 
+router.route('/:id').get(getSingleUser);
 module.exports = router;
